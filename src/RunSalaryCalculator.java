a4c567daae6815cf0b6e18b0ca04abbac1bb1439
Gia Huy <d2giahuy826@gmail.com>
2026-06-02 23:48:53 +0700

    add

--- src/RunSalaryCalculator.java
+++ src/RunSalaryCalculator.java
@@ -1,14 +1,26 @@
 import model.SalaryCalculator;
+import java.text.DecimalFormat;
 
 public class RunSalaryCalculator {
     public static void main(String[] args) {
         SalaryCalculator sc = SalaryCalculator.forFulltime(12000000, 10, 0).calculate();
         System.out.println(sc.toString());
-        System.out.println("Overtime pay: " + sc.getOvertimePay());
-        System.out.println("Absence deduction: " + sc.getAbsenceDeduction());
-        System.out.println("Attendance bonus: " + sc.getAttendanceBonus());
-        System.out.println("Gross salary: " + sc.getGrossSalary());
-        System.out.println("Tax amount: " + sc.getTaxAmount());
-        System.out.println("Net salary: " + sc.getNetSalary());
+
+        DecimalFormat df = new DecimalFormat("#,###");
+
+        // Format helper: round to nearest integer and apply grouping
+        java.util.function.Function<Double, String> fmt = v -> df.format(Math.round(v));
+
+        System.out.println("Overtime pay: " + fmt.apply(sc.getOvertimePay()));
+        System.out.println("Absence deduction: " + fmt.apply(sc.getAbsenceDeduction()));
+        System.out.println("Attendance bonus: " + fmt.apply(sc.getAttendanceBonus()));
+
+        // Round gross/net to nearest thousand, then format
+        long grossRounded = Math.round(sc.getGrossSalary() / 1000.0) * 1000;
+        long netRounded = Math.round(sc.getNetSalary() / 1000.0) * 1000;
+
+        System.out.println("Gross salary: " + df.format(grossRounded));
+        System.out.println("Tax amount: " + fmt.apply(sc.getTaxAmount()));
+        System.out.println("Net salary: " + df.format(netRounded));
     }
 }
