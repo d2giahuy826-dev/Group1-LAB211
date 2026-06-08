1e217aafb102feecbab207a82a8bd16ae43b3189
Vy <nguyennhattuongvy09122006@gmail.com>
2026-06-08 08:25:01 +0700

    Create CsvParseException.java

---
+++ src/model/CsvParseException.java
@@ -0,0 +1,11 @@
+package model;
+public class CsvParseException extends Exception {
+
+    public CsvParseException(String message) {
+        super(message);
+    }
+
+    public CsvParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
