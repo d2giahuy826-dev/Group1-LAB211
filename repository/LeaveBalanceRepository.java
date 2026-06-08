1f9a6323aa3531917f7ca3eaae1b6d57bf604ab4
Vy <nguyennhattuongvy09122006@gmail.com>
2026-06-08 08:24:18 +0700

    Create LeaveBalanceRepository.java

---
+++ repository/LeaveBalanceRepository.java
@@ -0,0 +1,27 @@
+package repository;
+
+import model.LeaveBalance;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeaveBalanceRepository {
+
+    private List<LeaveBalance> balances = new ArrayList<>();
+
+    public void add(LeaveBalance balance) {
+        balances.add(balance);
+    }
+
+    public LeaveBalance findByEmployeeId(String employeeId) {
+        for (LeaveBalance b : balances) {
+            if (b.getEmployeeId().equals(employeeId)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public List<LeaveBalance> getAll() {
+        return balances;
+    }
+}
