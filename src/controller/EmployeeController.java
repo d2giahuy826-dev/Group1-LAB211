d3114ecb3bca93fd056f231d29199bbcc73b8019
Vy <nguyennhattuongvy09122006@gmail.com>
2026-06-28 13:05:29 +0700

    Create EmployeeController.java

---
+++ src/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import model.Employee;
+import repository.EmployeeRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository = new EmployeeRepository();
+
+    public Employee findById(String empId) {
+        Optional<Employee> result = employeeRepository.findById(empId);
+        return result.orElse(null);
+    }
+
+    public List<Employee> findByDept(String deptId) {
+        return employeeRepository.findByField("deptId", deptId);
+    }
+
+    public void add(Employee employee) {
+        employeeRepository.add(employee);
+    }
+
+    public boolean update(Employee employee) {
+        return employeeRepository.update(employee);
+    }
+
+    public boolean delete(String empId) {
+        return employeeRepository.delete(empId);
+    }
+}
+    
+
